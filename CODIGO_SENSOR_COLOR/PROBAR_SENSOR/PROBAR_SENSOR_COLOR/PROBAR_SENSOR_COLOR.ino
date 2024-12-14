73245a20a1eab09f39c11388839dde2366b3d09d
Piero Gerardo Salas Izaguirre <gerardo.salas@pucp.edu.pe>
2024-12-14 15:54:36 -0500

    Update PROBAR_SENSOR_COLOR.ino

--- CODIGO_SENSOR_COLOR/PROBAR_SENSOR/PROBAR_SENSOR_COLOR/PROBAR_SENSOR_COLOR.ino
+++ CODIGO_SENSOR_COLOR/PROBAR_SENSOR/PROBAR_SENSOR_COLOR/PROBAR_SENSOR_COLOR.ino
@@ -1,6 +1,6 @@
-// Sensor de Color
-//
-//    Arduino   TCS230 SENSOR COLOR
+// Sensor de Color TCS3200
+// 
+//    Arduino   TCS3200 SENSOR COLOR
 //      8         OUT
 //      7         S3
 //      6         S2
@@ -15,54 +15,61 @@
 #define S2 4
 #define S3 8
 #define sensorSalida 9
-int Rojo_Frec = 0;
-int Verde_Frec = 0;
-int Azul_Frec = 0;
+
+// Parámetros de las lecturas
+#define NUM_SAMPLES 5  // Número de lecturas por color
+
+// Variables para las lecturas del sensor
+int redValue = 0, greenValue = 0, blueValue = 0;
+
 void setup() {
   pinMode(S0, OUTPUT);
   pinMode(S1, OUTPUT);
   pinMode(S2, OUTPUT);
   pinMode(S3, OUTPUT);
   pinMode(sensorSalida, INPUT);
-  
-                           // Configura la escala de Frecuencia en 20%
-  digitalWrite(S0,HIGH);
-  digitalWrite(S1,LOW);
-  
+
+  // Configura la escala de Frecuencia en 20%
+  digitalWrite(S0, HIGH);
+  digitalWrite(S1, LOW);
+
   Serial.begin(9600);
 }
+
 void loop() {
-                          // Configura el filtor ROJO para tomar lectura
-  digitalWrite(S2,LOW);
-  digitalWrite(S3,LOW);
-  delay(100);
-  Rojo_Frec= pulseIn(sensorSalida, LOW);
-  Serial.print(" R= "); Serial.print(Rojo_Frec);
-  delay(100);
-                            // Configura el filtor VERDE para tomar lectura
-  digitalWrite(S2,HIGH);
-  digitalWrite(S3,HIGH);
-  delay(100);
-  Verde_Frec = pulseIn(sensorSalida, LOW);
-  Serial.print(" V= "); Serial.print(Verde_Frec);
-  delay(100);
-                           // Configura el filtor AZUL para tomar lectura
-  digitalWrite(S2,LOW);
-  digitalWrite(S3,HIGH);
-  delay(100);
-  Azul_Frec = pulseIn(sensorSalida, LOW);
-  Serial.print(" A= "); Serial.print(Azul_Frec);
-  delay(100);
+  // Leer y promediar el valor ROJO
+  redValue = tomarPromedio(LOW, LOW);
 
-                  //Valores obtenidos en la calibracion del sensor, son propios de cada usuario 
-                  
- if (Rojo_Frec < 65 && Verde_Frec > 175 && Azul_Frec >125)  Serial.print(" . *** PELOTA ROJA **");
- if (Rojo_Frec > 130 && Verde_Frec >110 && Azul_Frec < 80)  Serial.print(" . *** PELOTA AZUL **");
- if (Rojo_Frec < 65 && Verde_Frec < 50 && Azul_Frec >40)  Serial.print(" . *** PELOTA VERDE **");
- if(Rojo_Frec > 95 && Verde_Frec > 90 && Azul_Frec >90) Serial.print(" . *** PELOTA NEGRO **");
- 
+  // Leer y promediar el valor VERDE
+  greenValue = tomarPromedio(HIGH, HIGH);
 
+  // Leer y promediar el valor AZUL
+  blueValue = tomarPromedio(LOW, HIGH);
 
- Serial.println("*");
- delay(200);
+  // Mostrar valores promedio
+  Serial.print("Promedio R: "); Serial.print(redValue);
+  Serial.print(" V: "); Serial.print(greenValue);
+  Serial.print(" A: "); Serial.println(blueValue);
+
+  // Clasificación del material con valores promedios
+  if ((redValue > 390 && redValue < 420) && (greenValue > 415 && greenValue < 440) && (blueValue > 330 && blueValue < 360)) Serial.print(" *** NEGRO ***");                 
+  if ((redValue > 240 && redValue < 280) && (greenValue > 300 && greenValue < 335) && (blueValue > 260 && blueValue < 290)) Serial.print(" *** CARTÓN ***");
+  if ((redValue > 290 && redValue < 400) && (greenValue > 340 && greenValue < 400) && (blueValue > 290 && blueValue < 345)) Serial.print(" *** PLASTICA ***");
+  if ((redValue > 35 && redValue < 40) && (greenValue > 47 && greenValue < 52) && (blueValue > 44 && blueValue < 49)) Serial.print(" *** VIDRIO ***");
+  if ((redValue > 35 && redValue < 40) && (greenValue > 47 && greenValue < 52) && (blueValue > 44 && blueValue < 49)) Serial.print(" *** OTROS ***");
+
+  Serial.println();
+  delay(200);
+}
+
+// Función para tomar el promedio de múltiples lecturas
+int tomarPromedio(int s2State, int s3State) {
+  long total = 0;
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    digitalWrite(S2, s2State);
+    digitalWrite(S3, s3State);
+    delay(10);  // Pequeño delay para estabilizar el sensor
+    total += pulseIn(sensorSalida, LOW);
+  }
+  return total / NUM_SAMPLES;
 }
